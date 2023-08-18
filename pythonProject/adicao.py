9f8ba69d0c8e9eb2ddfb23173a6cdc74aeecec4f
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-08-18 18:24:58 -0300

    python calculadoras

---
+++ pythonProject/adicao.py
@@ -0,0 +1,6 @@
+print('='*15 + ' Adição ' + '='*15)
+
+n1 = float(input('Digite o valor: '))
+n2 = float(input('Informe o valor {} + ' .format(n1)))
+
+print('O valor da soma de {} + {} = {}' .format(n1, n2, n1+n2))
---
+++ pythonProject/potencia.py
@@ -0,0 +1,6 @@
+print('='*15 + ' Multioliação ' + '='*15)
+
+n1 = float(input('Digite o valor: '))
+n2 = float(input('Informe o outro valor: '))
+
+print('O valor da multioliação de {} * {} = {:.2f}' .format(n1, n2, n1*n2))
