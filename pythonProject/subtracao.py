6585d583fb6f041c486b807a875685d67b0e3006
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-10-30 18:14:20 -0300

    atualizando o repositorio

--- pythonProject/subtracao.py
+++
@@ -1,6 +0,0 @@
-print('='*15 + ' Subtração ' + '='*15)
-
-n1 = float(input('Digite o valor: '))
-n2 = float(input('Informe o outro valor: '))
-
-print('O valor da subtração de {} - {} = {:.3f}' .format(n1, n2, n1-n2))
